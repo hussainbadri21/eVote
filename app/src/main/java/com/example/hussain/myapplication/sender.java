c30ef4bb137272d817ebd2b8ac3987a66806cb43
hussainHMC <hussainbadri786@gmail.com>
2017-04-01 06:06:33 +0530

    Connected to backend

--- app/src/main/java/com/example/hussain/myapplication/sender.java
+++ app/src/main/java/com/example/hussain/myapplication/sender.java
@@ -6,13 +6,33 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class sender extends AppCompatActivity {
 
+    String url = "http://evoter-hariaakash.rhcloud.com/voters/auth";
+    String uid;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +41,63 @@ public class sender extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Intent iin= getIntent();
+        Intent iin = getIntent();
 
         Bundle b = iin.getExtras();
 
-        if(b!=null)
-        {
-            String j =(String) b.get("br");
+        if (b != null) {
+            String j = (String) b.get("br");
+            String bi[] = j.split(" ");
+            for (String c : bi) {
+                Log.i("jj", c);
+            }
+             uid = bi[4].substring(5, bi[4].length() - 1);
 
-           StringTokenizer st=new StringTokenizer(j,"name");
-          String name=st.nextToken();
-            Toast.makeText(getApplicationContext(),name,Toast.LENGTH_SHORT).show();
+
+            Toast.makeText(getApplicationContext(), uid, Toast.LENGTH_LONG).show();
         }
 
-    }
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject person = new JSONObject(response);
+                            boolean ct = person.getBoolean("status");
+                            Log.e("ct value", Boolean.toString(ct));
+                            if (ct) {
+                                Toast.makeText(getApplicationContext(), "Aadhar Verified", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("Error", e.getMessage());
+                        }
+                        System.out.println(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //mTextView.setText("That didn't work!");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("uid", "1234");
+
+
+                Log.i("params of my service", params.toString());
+                return params;
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(10000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        RequestQueue requestQueue = Volley.newRequestQueue(sender.this);
+        requestQueue.add(stringRequest);
+
+    }
 }
+
